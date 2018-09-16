f92e788737a9f715e551dea9e5781fe67c6f70e9
Gparquet <parquet.geoffrey@gmail.fr>
2018-09-16 17:56:30 +0200

    feat: add operators file for live coding

---
+++ operateur/operateurs.js
@@ -0,0 +1,8 @@
+var user = { firstName: 'Phil', lastName: 'Jean'};
+if(user.firstName === "Phil" && user.lastName === "Jeano"){
+    console.log(user.firstName);
+}else{
+    console.log('Nom réalisé');
+};
+
+var philFirstName = user.firstName === 'Phil' ? true: false;
